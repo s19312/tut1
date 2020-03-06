a9bcebdfb6ea9d15a6d6291222e4fa08076c52c3
Roman Fedorchenko <s19312@pjwstk.edu.pl>
2020-03-06 14:46:19 +0200

    started task3

---
+++ tut1/tut1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace tut1
+{
+    public class Program
+    {
+       public  static async Task Main(string[] args)
+        {
+            var url = "https://www.pja.edu.pl";
+            var httpClient = new HttpClient();
+
+            var response = await httpClient.GetAsync(url);
+            
+
+
+            var regex = new Regex(@"[a-z][A-Z]@");
+            var content = response.Content;
+
+            var matches = regex.Matches(content.ToString());
+
+            foreach (var match in matches) {
+                Console.WriteLine(match.ToString());
+            }
+            Console.ReadLine();
+        }
+    }
+}
